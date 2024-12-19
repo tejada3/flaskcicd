46538b97fa3adca9126dfac6117a81f7d948e186
Jose Tejada <josetej3395@gmail.com>
2024-12-19 16:16:12 -0500

    edited requirments file

--- src/app.py
+++ src/app.py
@@ -4,18 +4,16 @@ app = Flask(__name__)
 
 
 @app.route("/")
-def hello_world():  # put application's code here
-    return "Hello World!"
+def hello_world(): return "Hello World!"
 
 
 @app.route("/jose")
-def hello_jose():
-    return "Hello Jose"
+def hello_jose():return "Hello Jose"
 
 
 @app.route("/coop")
 def hello_coop():  # put application's code here
-    return "Hello Coop"
+                                      return "Hello Coop"
 
 
 if __name__ == "__main__":
