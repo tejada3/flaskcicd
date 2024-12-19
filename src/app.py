353c3301d1bb126f9de2a20e0ed08283efbd80b9
Jose Tejada <josetej3395@gmail.com>
2024-12-19 14:46:42 -0500

    added /jose endpoint

--- src/app.py
+++ src/app.py
@@ -3,10 +3,15 @@ from flask import Flask
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route("/")
 def hello_world():  # put application's code here
-    return 'Hello World!'
+    return "Hello World!"
 
 
-if __name__ == '__main__':
+@app.route("/jose")
+def hello_jose():  # put application's code here
+    return "Hello Jose"
+
+
+if __name__ == "__main__":
     app.run()
